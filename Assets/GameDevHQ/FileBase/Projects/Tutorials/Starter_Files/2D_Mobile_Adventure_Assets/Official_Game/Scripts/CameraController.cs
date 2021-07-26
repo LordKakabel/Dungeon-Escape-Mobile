6a9efded043bf4ab73d4aaae1dc3649389a75511
LordKakabel <lord.kakabel@gmail.com>
2021-07-25 21:23:48 -0400

    Perspective Parallax
    Added perspective parallax effect.

---
+++ Assets/GameDevHQ/FileBase/Projects/Tutorials/Starter_Files/2D_Mobile_Adventure_Assets/Official_Game/Scripts/CameraController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public float _speed = 3f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 movement = new Vector3(Input.GetAxisRaw("Horizontal") * _speed * Time.deltaTime, 0, 0);
+        transform.Translate(movement);
+    }
+}
