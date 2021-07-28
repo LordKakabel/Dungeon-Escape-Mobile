04b6773745f070ac2580efe216fe33161ec0e5f0
LordKakabel <lord.kakabel@gmail.com>
2021-07-27 22:09:22 -0400

    Flip & Jump
    Player now flips to the correct position and jumps with proper animation.

--- Assets/GameDevHQ/FileBase/Projects/Tutorials/Starter_Files/2D_Mobile_Adventure_Assets/Official_Game/Scripts/Player.cs
+++ Assets/GameDevHQ/FileBase/Projects/Tutorials/Starter_Files/2D_Mobile_Adventure_Assets/Official_Game/Scripts/Player.cs
@@ -11,12 +11,27 @@ public class Player : MonoBehaviour
 
     private Rigidbody2D _rigidbody;
     private bool _isJumpResetNeeded = false;
+    private PlayerAnimation _animation;
+    private SpriteRenderer _sprite;
+    private bool _isGrounded = false;
 
     private void Awake()
     {
+        #region NullCheck
+
         _rigidbody = GetComponent<Rigidbody2D>();
         if (!_rigidbody)
             Debug.LogError(name + ": Rigidbody2D component not found!");
+
+        _animation = GetComponent<PlayerAnimation>();
+        if (!_animation)
+            Debug.LogError(name + ": PlayerAnimation script not found!");
+
+        _sprite = GetComponentInChildren<SpriteRenderer>();
+        if (!_sprite)
+            Debug.LogError(name + ": SpriteRenderer component not found in children!");
+
+        #endregion
     }
 
     void Update()
@@ -26,23 +41,50 @@ public class Player : MonoBehaviour
 
     private void Movement()
     {
-        float x = Input.GetAxisRaw("Horizontal") * _speed;
+        _isGrounded = IsGrounded();
 
-        _rigidbody.velocity = new Vector2(x, _rigidbody.velocity.y);
+        float horizontalMovement = Input.GetAxisRaw("Horizontal");
 
-        if (Input.GetButtonDown("Jump") && IsGrounded()) {
+        HandleFlip(horizontalMovement);
+
+        // Jump
+        if (Input.GetButtonDown("Jump") && _isGrounded)
+        {
             _rigidbody.velocity = new Vector2(_rigidbody.velocity.x, _jumpPower);
             StartCoroutine(ResetJumpRoutine());
+            _animation.Jumping(true);
+        }
+
+        _rigidbody.velocity = new Vector2(horizontalMovement * _speed, _rigidbody.velocity.y);
+
+        _animation.Move(horizontalMovement);
+    }
+
+    private void HandleFlip(float horizontalMovement)
+    {
+        if (horizontalMovement < 0)
+        {
+            _sprite.flipX = true;
+        }
+        else if (horizontalMovement > 0)
+        {
+            _sprite.flipX = false;
         }
     }
 
     private bool IsGrounded()
     {
         RaycastHit2D hitInfo = Physics2D.Raycast(transform.position, Vector2.down, _groundRayLength, _groundLayer);
+        Debug.DrawRay(transform.position, Vector2.down * _groundRayLength, Color.green, 0.1f);
 
         if (hitInfo.collider)
+        {
             if (!_isJumpResetNeeded)
+            {
+                _animation.Jumping(false);
                 return true;
+            }
+        }
 
         return false;
     }
---
+++ Assets/GameDevHQ/FileBase/Projects/Tutorials/Starter_Files/2D_Mobile_Adventure_Assets/Official_Game/Scripts/PlayerAnimation.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour
+{
+    private Animator _animator;
+
+    private void Awake()
+    {
+        _animator = GetComponentInChildren<Animator>();
+        if (!_animator)
+            Debug.LogError(name + ": Animator component not found!");
+    }
+
+    public void Move(float move)
+    {
+        _animator.SetFloat("Move", Mathf.Abs(move));
+    }
+
+    public void Jumping(bool isJumping)
+    {
+        _animator.SetBool("IsJumping", isJumping);
+    }
+}
