30452b6e0652d04b99397eec5e2f65354abadcff
LordKakabel <lord.kakabel@gmail.com>
2021-08-09 22:25:21 -0400

    Ads
    Test ads play from shop.

---
+++ Assets/GameDevHQ/FileBase/Projects/Tutorials/Starter_Files/2D_Mobile_Adventure_Assets/Official_Game/Scripts/UI/AdsManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Advertisements;
+
+public class AdsManager : MonoBehaviour
+{
+    public void ShowRewardedAd()
+    {
+        // Check if ad is ready
+        if (Advertisement.IsReady("Rewarded_Android"))
+        {
+            var options = new ShowOptions
+            {
+                resultCallback = HandleShowResult
+            };
+
+            Advertisement.Show("Rewarded_Android", options);
+        }
+    }
+
+    private void HandleShowResult(ShowResult result)
+    {
+        switch (result)
+        {
+            case ShowResult.Failed:
+                Debug.Log("Ad failed.");
+                break;
+            case ShowResult.Skipped:
+                Debug.Log("Ad was skipped.");
+                break;
+            case ShowResult.Finished:
+                Debug.Log("Ad viewed. +100 D.");
+                break;
+            default:
+                break;
+        }
+    }
+}
