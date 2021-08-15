d77289fd504d8d187521491e3b1fae748323d889
LordKakabel <lord.kakabel@gmail.com>
2021-08-15 16:05:22 -0400

    Android Store
    Ready to deplaoy to Android Play Store.

---
+++ Assets/GameDevHQ/FileBase/Projects/Tutorials/Starter_Files/2D_Mobile_Adventure_Assets/Official_Game/Scripts/MainMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField] private int _indexOfFirstScene = 1;
+
+    public void StartButton()
+    {
+        SceneManager.LoadScene(_indexOfFirstScene);
+    }
+
+    public void QuitButton()
+    {
+        Application.Quit();
+    }
+}
