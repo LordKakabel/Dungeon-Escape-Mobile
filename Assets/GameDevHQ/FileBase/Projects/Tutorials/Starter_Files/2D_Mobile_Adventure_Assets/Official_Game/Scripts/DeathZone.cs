577b5d6df1579fd52f7fac6c85fd5f92df2ab781
LordKakabel <lord.kakabel@gmail.com>
2021-09-19 16:34:35 -0400

    Death Zone & Respawn Point

---
+++ Assets/GameDevHQ/FileBase/Projects/Tutorials/Starter_Files/2D_Mobile_Adventure_Assets/Official_Game/Scripts/DeathZone.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathZone : MonoBehaviour
+{
+    [SerializeField] private Transform _respawnPoint = null;
+
+    private void Start()
+    {
+        if (!_respawnPoint)
+            Debug.LogError(name + ": Respawn Point Transform not assigned!");
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            collision.transform.position = _respawnPoint.transform.position;
+        }
+    }
+}
